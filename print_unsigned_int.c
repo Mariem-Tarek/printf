90bac8c96a3c1b04d1bdd26a0cd878007b9c441e
Maya1795 <mayasameh18@gmail.com>
2023-10-17 13:05:38 -0700

    test 1

--- print_unsigned_int.c
+++ print_unsigned_int.c
@@ -14,11 +14,9 @@ int print_un_i(int x)
 	int m = x;
 
 	if (m < 0)
-	{
 		m = -m;
-	}
-	if (m / 10)
+	if ((m / 10) > 0)
 		print_un_i(m / 10);
-	_putchar2(m % 10 + '0');
-	return (count(m));
+	_putchar2((m % 10) + '0');
+	return (count(x));
 }
