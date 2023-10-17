dc562a7f3313129f05a0f1a238faf9f7e3af03f8
Mariem Tarek <mariemtarek283@gmail.com>
2023-10-17 09:12:23 +0300

    main.h

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include<stdio.h>
+#include<stdarg.h>
+#include<unistd.h>
+#include <stdlib.h>
+
+
+int _putchar2(char c);
+int _puts2(char *c);
+int _printf(const char *format, ...);
+int print_int(va_list p);
+int print_d(va_list p);
+int print_binary(va_list p);
+
+#endif
